de6826a6c81a15a525f2fbfdeb950f9998a0e287
Matt Maribojoc <matthewmaribojoc@gmail.com>
2025-02-07 16:26:21 -0500

    repo setup

---
+++ src/module.ts
@@ -0,0 +1,20 @@
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { consola } from 'consola'
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'my-module',
+    configKey: 'myModule',
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup(_options, _nuxt) {
+    consola.info('my-module module setup')
+    const resolver = createResolver(import.meta.url)
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addPlugin(resolver.resolve('./runtime/plugin'))
+  },
+})
