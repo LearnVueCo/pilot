f4466f0b04b586cdfbad8f4fb50a6072ff0f6411
Matt Maribojoc <matthewmaribojoc@gmail.com>
2025-02-10 12:20:59 -0500

    markdown

--- src/runtime/components/Editor.vue
+++ src/runtime/components/Editor.vue
@@ -4,6 +4,7 @@ import StarterKit from '@tiptap/starter-kit'
 import Placeholder from '@tiptap/extension-placeholder'
 import GlobalDragHandle from 'tiptap-extension-global-drag-handle'
 import Image from '@tiptap/extension-image'
+import { Markdown } from 'tiptap-markdown'
 import Command from '../extensions/command'
 import suggestion from '../extensions/suggestion'
 
@@ -32,12 +33,23 @@ const editor = useEditor({
       inline: true,
       allowBase64: true,
     }),
+    Markdown.configure({
+      html: true, // Allow HTML input/output
+      tightLists: true, // No <p> inside <li> in markdown output
+      tightListClass: 'tight', // Add class to <ul> allowing you to remove <p> margins when tight
+      bulletListMarker: '-', // <li> prefix in markdown output
+      linkify: false, // Create links from "https://..." text
+      breaks: false, // New lines (\n) in markdown input are converted to <br>
+      transformPastedText: true, // Allow to paste markdown text in the editor
+      transformCopiedText: true, // Copied text is transformed to markdown
+    }),
   ],
 })
 </script>
 
 <template>
   <div style="min-height: 100vh; padding: 4rem">
+    <slot name="header" :editor="editor" />
     <bubble-menu
       :editor="editor"
       :tippy-options="{ duration: 100 }"
